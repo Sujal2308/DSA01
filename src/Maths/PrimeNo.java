3efe1bc9d859d33ac0195b201fc7d37307856da1
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-05-01 07:49:07 +0000

    Prime Number Optimized Approach

--- src/Maths/PrimeNo.java
+++ src/Maths/PrimeNo.java
@@ -1,13 +1,25 @@
 class PrimeNo{
   public static void main(String[] args){
-    int num = 2;
+    int num = 38;
     System.out.println(isPrime(num));
   }
   static boolean isPrime(int num){
-    if(num==2){
-      return true;
-    }
-    for(int i = 2 ; i<7 ; i++){
+    // Brute Force
+    // if(num==2){
+    //   return true;
+    // }
+    // for(int i = 2 ; i<num ; i++){
+    //   if(num % i == 0){
+    //     return false;
+    //   }
+    // }
+    // return true;
+
+    // Optimal Solution
+    for(int  i= 2 ; i*i<=num ; i++){
+      if(num<=1){
+        return false;
+      }
       if(num % i == 0){
         return false;
       }
