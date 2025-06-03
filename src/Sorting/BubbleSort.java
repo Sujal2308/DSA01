22b513958a149d73ed331f60b21986b9abee9285
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-06-03 05:38:15 +0000

    Bubble sorting on arrays

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class BubbleSort{
+  public static void main(String[] args){
+    int[] arr = {1,2,3,4,5,-1,0};
+    // inplace sorting
+    // stable sorting
+    // time complexity = O(n^2)
+    // space complexity = O(1)
+    // opposite of selection sort
+    bubbleSort(arr);
+  }
+  static void bubbleSort(int[] arr){
+    for(int i = 0 ; i<arr.length; i++){
+      for(int j = 1 ; j<arr.length-i ; j++){
+        if(arr[j]<arr[j-1]){
+          int temp = arr[j-1];
+          arr[j-1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(arr));
+  }
+}
