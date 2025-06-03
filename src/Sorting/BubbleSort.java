c8220d28daf7a3aa34132ba338725a3fd74d5b32
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-06-03 05:46:34 +0000

    optimized bubble sort for already sorted arrays

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
-class BubbleSort{
-  public static void main(String[] args){
-    int[] arr = {1,2,3,4,5,-1,0};
+
+class BubbleSort {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, -1, 0 };
     // inplace sorting
     // stable sorting
     // time complexity = O(n^2)
@@ -9,14 +10,23 @@ class BubbleSort{
     // opposite of selection sort
     bubbleSort(arr);
   }
-  static void bubbleSort(int[] arr){
-    for(int i = 0 ; i<arr.length; i++){
-      for(int j = 1 ; j<arr.length-i ; j++){
-        if(arr[j]<arr[j-1]){
-          int temp = arr[j-1];
-          arr[j-1] = arr[j];
+
+  static void bubbleSort(int[] arr) {
+    boolean swap = false;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length - i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j - 1];
+          arr[j - 1] = arr[j];
           arr[j] = temp;
+          swap = true;
         }
+
+      }
+      if (!swap) {
+        break;
+      }else{
+        swap = false;
       }
     }
     System.out.println(Arrays.toString(arr));
