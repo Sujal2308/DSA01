457d949610763f74e375fc2c69dc5ce4a7d2fa88
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-06-03 05:13:51 +0000

    Insertion sort of an array

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class InsertionSort{
+  public static void main(String[] args){
+    int[] arr = {5,3,4,1,2};
+    // Inplace sorting algorithm
+    // time complexity = O(n^2)
+    insertionSort(arr);
+  }
+
+  static void insertionSort(int[] arr){
+    for(int i = 0 ; i<=arr.length-2 ; i++){
+      int min = i;
+      for(int j = i+1 ; j<=arr.length-1 ; j++){
+        if(arr[j]<arr[min]){
+          min = j;
+        }
+      }
+      int temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+    System.out.println(Arrays.toString(arr));
+  }
+}
