e05f7f0ed648413b3fdd66a578271dc14fb2c2a7
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-06-08 09:03:04 +0000

    added comments in two sum

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -1,30 +1,29 @@
-class TwoSum{
-  public static void main(String[] args){
-    int[] arr = {2,7,11,15};
+class TwoSum {
+  public static void main(String[] args) {
+    int[] arr = { 2, 7, 11, 15 };
     int target = 6;
-    int[] result = twoSum(arr,target);
-    System.out.println(result[0]+" "+result[1]);
+    int[] result = twoSum(arr, target);
+    System.out.println(result[0] + " " + result[1]);
   }
 
-  static int[] twoSum(int[] arr , int target){
+  static int[] twoSum(int[] arr, int target) {
+    // Only better if you want to return the elements not indexes
     // Time complexity : O(nlogn)
     // Space complexity : O(1);
     Arrays.sort(arr); // T.c : O(nlogn)
-       int low = 0 ;
-       int  high = arr.length-1;
-       int sum = 0;
-       while(low<high){ // T.c : O(n) less dominant
-           sum = arr[low] + arr[high];
-           if(sum==target){
-               return new int[]{low ,high};
-           }
-           else if(sum>target){
-               high--;
-           }
-           else{
-               low++;
-           }
-       }
-       return new int[]{-1,-1};
+    int low = 0;
+    int high = arr.length - 1;
+    int sum = 0;
+    while (low < high) { // T.c : O(n) less dominant
+      sum = arr[low] + arr[high];
+      if (sum == target) {
+        return new int[] { arr[low], arr[high] };
+      } else if (sum > target) {
+        high--;
+      } else {
+        low++;
+      }
     }
+    return new int[] { -1, -1 };
+  }
 }
