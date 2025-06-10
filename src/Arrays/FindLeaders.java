d01d63a622b98b625f9dc95ae8624bfd5d8a1946
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-06-10 03:49:17 +0000

    added comments in leaders problem

--- src/Arrays/FindLeaders.java
+++ src/Arrays/FindLeaders.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
 
-class FindLeaders{
-  public static void main(String[] a){
-    int[] arr = {16,17,4,3,5,2};
-   
-    System.out.println( findLeaders(arr));
+// Leaders in an array are the elements that are greater than all the elements to their right side.
+// And the rightmost(last) element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2.
+
+class FindLeaders {
+  public static void main(String[] a) {
+    int[] arr = { 16, 17, 4, 3, 5, 2 };
+
+    System.out.println(findLeaders(arr));
   }
 
-  static ArrayList<Integer> findLeaders(int[] arr){
-     int n = arr.length;
+  static ArrayList<Integer> findLeaders(int[] arr) {
+    // Time Complexity O(n)
+    // Space Complexity O(n)
+    int n = arr.length;
     ArrayList<Integer> list = new ArrayList<>();
-   
+
     int max = Integer.MIN_VALUE;
-    for(int i = n-1 ; i>=0 ; i--){
-      if(arr[i]>max){
+    for (int i = n - 1; i >= 0; i--) {
+      if (arr[i] > max) {
         max = arr[i];
         list.add(max);
       }
