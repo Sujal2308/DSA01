7a4e3769c83740dec6c9372fc2a45f0fb28e2b7a
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-07-21 19:23:13 +0000

    ADDED comments

--- src/Arrays/BinarySearch/firstAndLastOccurances.java
+++ src/Arrays/BinarySearch/firstAndLastOccurances.java
@@ -1,4 +1,8 @@
 import java.util.Arrays;
+// Given a sorted array arr containing n elements with possibly duplicate elements, the task is to find indexes of first and last occurrences of an element x in the given array.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class firstAndLastOccurances{
   public static void main(String[] args){
     int[] arr =  {1,2,3,3,3,3,4,5,6,7,8,9};
