822e278ba4ef7ff54735d3006a7805de2b4548cc
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-07-22 16:15:06 +0000

    added comments

--- src/Arrays/BinarySearch/SearchInRotatedSorted.java
+++ src/Arrays/BinarySearch/SearchInRotatedSorted.java
@@ -1,4 +1,7 @@
 class RotatedSorted{
+  // Search in Rotated Sorted Array
+  // Time Complexity : O(logn)
+  // Space Complexity : O(1)
     public static void main(String[] args){
         int[] arr = {4,5,6,7,0,1,2};
         System.out.println(search(arr, 0));
