f7b46adb5fea19145bb81897a8c3a2ba0c5608f2
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-07-22 16:14:10 +0000

    search in rotated and sorted array using binary search

---
+++ src/Arrays/BinarySearch/SearchInRotatedSorted.java
@@ -0,0 +1,34 @@
+class RotatedSorted{
+    public static void main(String[] args){
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(search(arr, 0));
+    }
+  static int search(int[] nums, int target) {
+      int low = 0;
+      int high = nums.length - 1;
+      while (low <= high) {
+          int mid = (low + high) / 2;
+
+          if (nums[mid] == target) {
+              return mid;
+          }
+
+          if (nums[mid] >= nums[low]) {
+              //? left part is sorted
+              if (target >= nums[low] && target <= nums[mid]) {
+                  high = mid - 1;
+              } else {
+                  low = mid + 1;
+              }
+          } else if (nums[mid] <= nums[high]) {
+              //? right part is sorted
+              if (target >= nums[mid] && target <= nums[high]) {
+                  low = mid + 1;
+              } else {
+                  high = mid - 1;
+              }
+          }
+      }
+      return -1;
+  }
+}
