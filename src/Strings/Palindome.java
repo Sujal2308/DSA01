8fde8e399068c3a0c4917fdf5b587dde7011a1ff
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-08-19 14:12:25 +0000

    check if string palindrome (avoiding spaces, punctuations)

---
+++ src/Strings/Palindome.java
@@ -0,0 +1,27 @@
+public class Palindome {
+  public static void main(String[] args) {
+    String s = "A man, a plan, a canal: Panama";
+    System.out.println(isPalindrome(s));
+  }
+
+  public static boolean isPalindrome(String str) {
+    int low = 0;
+    int high = str.length() - 1;
+    str = str.toLowerCase();
+    while (low < high) {
+      if (str.charAt(low) < 'a' || str.charAt(low) > 'z') {
+        low++;
+        continue;
+      } else if (str.charAt(high) < 'a' || str.charAt(high) > 'z') {
+        high--;
+        continue;
+      }
+      if (str.charAt(low) != str.charAt(high)) {
+        return false;
+      }
+      low++;
+      high--;
+    }
+    return true;
+  }
+}
