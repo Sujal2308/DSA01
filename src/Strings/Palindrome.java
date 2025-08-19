0f22960fee27052798eee37dc47450200d53a0dd
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-08-19 14:12:46 +0000

    renaming the file name

--- src/Strings/Palindrome.java
+++ src/Strings/Palindrome.java
@@ -1,4 +1,4 @@
-public class Palindome {
+public class Palindrome {
   public static void main(String[] args) {
     String s = "A man, a plan, a canal: Panama";
     System.out.println(isPalindrome(s));
