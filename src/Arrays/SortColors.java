fdc1e02e069f24da6b24ac9a7bd617c80ce499ef
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-08-27 15:41:23 +0000

    sort array of 0's 1's and 2's

---
+++ src/Arrays/SortColors.java
@@ -0,0 +1,39 @@
+class SortColors{
+
+    public static void main(String[] args){
+        int[] arr = {2,0,2,1,1,0};
+        sortColors(arr);
+    }
+    public static void sortColors(int[] nums){
+      int counter = 0;
+
+      // sort 0's
+      for(int i =0; i<nums.length ; i++){
+          if(nums[i]==0){
+              int temp = nums[counter];
+              nums[counter] = nums[i];
+              nums[i] =temp;
+              counter++;
+          }
+      }
+      //sort 1's
+      for(int j = 0 ; j<nums.length ; j++){
+          if(nums[j]==1){
+              int temp = nums[counter];
+              nums[counter] = nums[j];
+              nums[j] =temp;
+              counter++;
+          }
+      }
+      // sort 2's
+      for(int k = 0; k<nums.length ; k++){
+          if(counter< nums.length-1 && nums[k]==2){
+              int temp = nums[counter];
+              nums[counter] = nums[k];
+              nums[k] =temp;
+              counter++;
+          }
+      }
+      System.out.println(Arrays.toString(nums));
+    }
+}
