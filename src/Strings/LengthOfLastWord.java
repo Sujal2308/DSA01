3153102ac4d7b6d3dbd7ff0c40e6ce6d358a3f15
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-08-29 08:22:23 +0000

    modified the comments

--- src/Strings/LengthOfLastWord.java
+++ src/Strings/LengthOfLastWord.java
@@ -1,4 +1,7 @@
 class LengthOfLastWord{
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
+  // The last word is the last string in the array that is not empty and does not contain any leading or trailing spaces.
     public static void main(String[] args){
         String s = "Hello World";
         System.out.println(lengthOfLastWord(s));
