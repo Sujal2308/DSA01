fc58badab15d729a65d19a633d7765e3c43ba7d1
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2026-01-22 20:56:04 +0530

    Add NumberOf1Bits.java to count set bits in integer

---
+++ src/Maths/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+    public int hammingWeight(int n) {
+        String s = Integer.toBinaryString(n);
+        int count1 = 0;
+        for(char c : s.toCharArray()){
+            if(c=='1'){
+                count1++;
+            }
+        }
+        return count1;
+    }
+}
