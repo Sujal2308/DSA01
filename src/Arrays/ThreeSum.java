138efec499056dccde0d9ab4e239850c1378dc88
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2026-02-15 14:44:33 +0530

    Implement 3 sum function to find triplets

---
+++ src/Arrays/ThreeSum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+             List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int p1 = i + 1;
+            int p2 = nums.length - 1;
+
+            while (p1 < p2) {
+
+                int sum = nums[i] + nums[p1] + nums[p2];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
+
+                    // Skip duplicate p1
+                    while (p1 < p2 && nums[p1] == nums[p1 + 1]) p1++;
+
+                    // Skip duplicate p2
+                    while (p1 < p2 && nums[p2] == nums[p2 - 1]) p2--;
+
+                    p1++;
+                    p2--;
+                }
+                else if (sum < 0) {
+                    p1++;
+                } 
+                else {
+                    p2--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
