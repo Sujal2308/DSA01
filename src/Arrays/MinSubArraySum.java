c35bdb4cdaa2f867ab8e8e72b760d80e7552660f
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2026-02-19 21:59:17 +0530

    Implement minSubArrayLen method for minimum subarray

---
+++ src/Arrays/MinSubArraySum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+         int low = 0;
+         int high = 0;
+         int min = Integer.MAX_VALUE;
+         int sum = 0;
+
+         while(high<nums.length){
+            sum = sum + nums[high];
+
+            while(sum>=target){
+                int length = high-low+1;
+                min = Math.min(length,min);
+                sum =  sum - nums[low];
+                low++;;
+            }
+            high++;
+         }
+         return min==Integer.MAX_VALUE? 0 : min;
+    }
+}
