b1d41ffa15f67ea5342b13d0c562ef90d24730e6
Sujal Bhugul <sujalbhugul08@gmail.com>
2026-05-22 19:55:30 +0530

    Add solution for Two Sum II problem

---
+++ src/Arrays/TwoPointer/TwoSumII.java
@@ -0,0 +1,21 @@
+// LeetCode Problem: 167. Two Sum II - Input Array Is Sorted
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 }; // Return 1-based indices
+            } else if (sum < target) {
+                left++; // Move left pointer to the right
+            } else {
+                right--; // Move right pointer to the left
+            }
+        }
+
+        return new int[] { -1, -1 }; // Return -1 if no solution is found
+    }
+}
